d26652744ef550ae0c4db22c5d686a9f9a71e24c
YuryKoropenko <yurykoropenko@gmail.com>
2018-06-13 17:59:41 +0300

    index

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function() {
 	
+	$('.h-nav__button').on('click', function() {
+		$('.h-nav').fadeIn();
+		$('body').css({
+			'overflow':'hidden'
+		});
+		return false;
+	});
+
+	$('.h-nav__close').on('click', function() {
+		$('.h-nav').fadeOut();
+		$('body').css({
+			'overflow':'auto'
+		});
+		return false;
+	});
+
 });
