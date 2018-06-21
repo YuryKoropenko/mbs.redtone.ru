727ee35541a4bdfe27de37404c81fae007a9fbff
YuryKoropenko <yurykoropenko@gmail.com>
2018-06-21 17:49:20 +0300

    new commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,26 @@
 $(function() {
 	
+	$('.p-filter__item label').on('click', function() {
+		$(this).children('svg').toggleClass('fa-square');
+		$(this).children('svg').toggleClass('fa-check-square');
+	});
+	$('.p-filter__title').on('click', function() {
+		$(this).parent('.p-filter__cat').toggleClass('show');
+		$(this).parent('.p-filter__cat').children('.p-filter__list').slideToggle();
+		$(this).parent('.p-filter__cat').children('.p-filter__block').slideToggle();
+		return false;
+	});
+
+	$('.p-filter__mobbtn').on('click', function() {
+		$(this).toggleClass('active');
+		$('.p-filter').slideToggle();
+		return false;
+	});
+	$('.p-filter__search').on('click', function() {
+		$('.page__p-search').slideToggle();
+		return false;
+	});
+
 	$('.h-nav__button').on('click', function() {
 		$('.header__right').fadeIn();
 		$('body').css({
