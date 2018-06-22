46bb7f9ebcd51b94fb984f7b95b98acf00f73754
YuryKoropenko <yurykoropenko@gmail.com>
2018-06-22 17:00:42 +0300

    new-commit

--- app/js/main.js
+++ app/js/main.js
@@ -21,6 +21,15 @@ $(function() {
 		return false;
 	});
 
+	$('.p-sort__link').on('click', function() {
+		$(this).parent('li').toggleClass('active');
+		$(this).parent('li').children('.p-sort__hidden').stop(true, false).slideToggle();
+		return false;
+	});
+
+	var wt = $('.p-participant__top').width();
+	$('.p-participant__top').height(wt);
+
 	$('.h-nav__button').on('click', function() {
 		$('.header__right').fadeIn();
 		$('body').css({
@@ -37,4 +46,51 @@ $(function() {
 		return false;
 	});
 
-});
+
+	let canvas = document.querySelector('canvas')
+	let ctx = canvas.getContext("2d")
+	canvas.width = canvas.offsetWidth
+	canvas.height = canvas.offsetHeight
+	let dim = 8
+	let ratio = .43
+	let dimOffset = dim
+	let maxWidth = canvas.width / (dim * ratio)
+	let maxHeight = canvas.height / dim
+	var fps = 30;
+	let interval = 1000/fps
+	let then = Date.now()
+
+	ctx.font = "normal " + dim + "px Montserrat"
+
+	function render(){
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		for(let i = 0; i < maxHeight; i++){
+			ctx.fillText(  randomString(maxWidth, "aA#!"), 0, (i * dim) + dimOffset)
+		}
+	}
+
+	function randomString(length, chars = "aA#!") {
+		var mask = ''
+		if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz'
+			if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+				if (chars.indexOf('#') > -1) mask += '0123456789'
+					if (chars.indexOf('!') > -1) mask += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\'
+				var result = ''
+				for (var i = length; i > 0; --i) result += mask[Math.floor(Math.random() * mask.length)]
+					return result
+			}
+
+
+			function update() {
+				let now = Date.now()
+				let delta = now - then
+				if (delta >= interval) {
+					then = now - (delta % interval)
+					render()
+				}
+				window.requestAnimationFrame(update)
+			}
+			window.requestAnimationFrame(update)
+
+
+		});
